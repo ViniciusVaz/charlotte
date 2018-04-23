aec796231dd668a87fa443aaa1b9e7d6cc3dfef6
Vinicius <viniciushenriquevaz@hotmail.com>
2018-04-23 10:31:36 -0300

    - Heroku configuration

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+    , path = require("path")
+    , serveStatic = require("serve-static")
+    , port = process.env.PORT || 4004
+    , app = express()
+
+app.use(serveStatic(path.join(__dirname, "dist")))
+
+app.listen(port, () => {
+    console.log(`Server started: ${port}`)
+})
